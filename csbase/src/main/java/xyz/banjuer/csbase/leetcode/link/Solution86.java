5beb96459ba56ccef294844c4ddca97e1d579f96
banjuer <banjuer@outlook.com>
2021-05-24 20:13:46 +0800

    86

--- csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution86.java
+++ csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution86.java
@@ -1,4 +1,50 @@
 package xyz.banjuer.csbase.leetcode.link;
 
+import xyz.banjuer.common.entity.ListNode;
+import xyz.banjuer.common.utils.ListNodeUtils;
+
 public class Solution86 {
+
+    /**
+     * 86. 分隔链表
+     *
+     * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+     * 你应当 保留 两个分区中每个节点的初始相对位置。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/partition-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public ListNode partition(ListNode head, int x) {
+        // 保存两个链表，分别代表大于等于x和小于x的链表，随后将小链表的尾部指向大链表即可
+        ListNode small = new ListNode();
+        ListNode large = new ListNode();
+
+        ListNode smallTail = small;
+        ListNode largeTail = large;
+        while (head != null) {
+            if (head.val >= x) {
+                largeTail.next = head;
+                largeTail = largeTail.next;
+                head = head.next;
+                // 当前尾部节点需指向空
+                largeTail.next = null;
+            } else {
+                smallTail.next = head;
+                smallTail = smallTail.next;
+                head = head.next;
+                smallTail.next = null;
+            }
+
+        }
+        smallTail.next = large.next;
+        return small.next;
+    }
+
+    public static void main(String[] args) {
+        Solution86 solution86 = new Solution86();
+        ListNode link = ListNodeUtils.createLink(new int[]{1, 4, 3, 2, 5, 2});
+        ListNodeUtils.printLink(solution86.partition(link, 3));
+    }
+
 }
--- csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution92.java
+++ csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution92.java
@@ -1,4 +1,17 @@
 package xyz.banjuer.csbase.leetcode.link;
 
+import xyz.banjuer.common.entity.ListNode;
+
 public class Solution92 {
+
+    /**
+     * 92. 反转链表 II
+     *
+     * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+     * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+    }
+
 }
