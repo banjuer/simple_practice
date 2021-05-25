e118a0776d2fa80692f5f1c394fee2acc3332e22
banjuer <banjuer@outlook.com>
2021-05-25 21:03:03 +0800

    链表反转

--- csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution206.java
+++ csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution206.java
@@ -1,7 +1,39 @@
 package xyz.banjuer.csbase.leetcode.link;
 
+import xyz.banjuer.common.entity.ListNode;
+import xyz.banjuer.common.utils.ListNodeUtils;
+
 public class Solution206 {
 
+    /**
+     * 206. 反转链表
+     *
+     * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+     */
+    public ListNode reverseList(ListNode head) {
+        // 已反转的链表
+        ListNode resvered = null;
+        if (head == null) {
+            return null;
+        }
+        // 下一个节点
+        ListNode next = head.next;
+        while (head != null) {
+            head.next = resvered;
+            resvered = head;
+            head = next;
+            if (head != null) {
+                next = head.next;
+            }
 
+        }
+        return resvered;
+    }
+
+    public static void main(String[] args) {
+        Solution206 solution206 = new Solution206();
+        ListNode link = ListNodeUtils.createLink(new int[]{1, 2, 3, 4, 5});
+        ListNodeUtils.printLink(solution206.reverseList(link));
+    }
 
 }
--- csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution92.java
+++ csbase/src/main/java/xyz/banjuer/csbase/leetcode/link/Solution92.java
@@ -11,7 +11,33 @@ public class Solution92 {
      * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
      */
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
 
+        // 遍历的索引位
+        int index = 1;
+        ListNode beforeLeft = null;
+        while (index < left) {
+            beforeLeft = head;
+            head = head.next;
+            index ++;
+        }
+        // 此时 index == left
+        beforeLeft.next = null;
+        // 开始反转链表
+        ListNode pre = null;
+        ListNode lNode = head;
+        ListNode next = head.next;
+        while (index <= right) {
+            head.next = pre;
+            pre = head;
+            next = head.next;
+        }
+        ListNode rNode = next;
+
+        beforeLeft.next = rNode;
+        lNode.next = head.next;
+        return dummy.next;
     }
 
 }
